680f59bb66bc6171dde0bdd9a418a62dbb5a27de
rashed967 <rashedhasan967@gmail.com>
2025-07-24 20:35:29 +0600

    added api key

--- index.tsx
+++ index.tsx
@@ -7,7 +7,7 @@
 import {GoogleGenAI} from '@google/genai';
 import * as marked from 'marked';
 
-const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
+const GEMINI_API_KEY = 'AIzaSyCigGG7RBj8K-1L5IXovPRNJahjoYTn4B8';
 
 async function debug(...args: string[]) {
   const turn = document.createElement('div');
